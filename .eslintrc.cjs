8b17c3cfaa73cc07b3e8f8dda50065dfcd6ba96e
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-07-02 17:51:19 +0300

    init: start components task

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    'prettier',
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ['react-refresh', 'react-compiler', 'react', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-compiler/react-compiler': 'error',
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
