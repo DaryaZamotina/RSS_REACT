77840df1e2f1afb8bb02b855fc0b06333ce9e203
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-07-03 17:04:08 +0300

    feat: add husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react-compiler', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
