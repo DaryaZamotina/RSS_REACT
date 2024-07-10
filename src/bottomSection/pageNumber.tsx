a9ca1dbebf750b5e29fc09b97570e7420f925352
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-07-10 22:10:43 +0300

    fix: delete all files

--- src/bottomSection/pageNumber.tsx
+++
@@ -1,8 +0,0 @@
-import { Component } from 'react';
-import { Counter } from './counter';
-
-export default class PageWrapper extends Component {
-  render() {
-    return <div id="pageWrapper">Page number: {Counter.getCount()}</div>;
-  }
-}
